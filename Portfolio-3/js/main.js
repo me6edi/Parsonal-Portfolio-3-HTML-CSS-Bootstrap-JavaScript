279e4840e63f5b672edb0bb8b35a5474b6694576
Mehedi Amin <exited17@gmail.com>
2020-02-20 16:45:50 +0600

    slider-area

--- Portfolio-3/js/main.js
+++ Portfolio-3/js/main.js
@@ -24,7 +24,10 @@ $(window).on('scroll', function () {
 	}
 });
 
-
+// data - background
+$("[data-background]").each(function () {
+	$(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
+})
 
 // mainSlider
 function mainSlider() {
